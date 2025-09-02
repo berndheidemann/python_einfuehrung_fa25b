a433545a60b0f7e371249cea8c866b55e2410c06
Bernd Heidemann <bernd.heidemann@gmail.com>
2025-09-02 14:57:40 +0200

    Anfangsaufgaben 4

--- anfangsaufgaben1/Aufgabe4.py
+++ anfangsaufgaben1/Aufgabe4.py
@@ -1,4 +1,14 @@
-
 import math
 
-math.sqrt(9)
+g = 9.81
+s= 80
+t_squared = 2 * s / g
+t = math.sqrt(t_squared)      # math.sqrt zieht die Wurzel einer Zahl
+
+print("Zeit bis Aufprall: ", t)
+
+speed_m_s=t*g
+print("Aufprallgeschwindigkeit in m/s: ", speed_m_s)
+
+speed_kmh=speed_m_s*3.6
+print("Aufprallgeschwindigkeit in kmh: ", speed_kmh)
